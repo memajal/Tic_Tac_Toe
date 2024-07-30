d3804344f2df7d7abfe2b48c2444647fe512cd70
Alketa Memaj <memajalketa92@gmail.comgit config --global user.email memajalketa92@gmail.comgit config --global user.name Alketa>
2024-07-30 11:40:03 +0200

    Create the startGame module that will create the playerObjects and will call the crateBoard function
    The factory function cratePlayers with the name and symbol of the player; the StartGame module, will have a object of the players, and a function of start game; start function crate the player Objects with the factory function and popullat the array, call the createGameBoard of the module GameBoard

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -3,7 +3,6 @@
 /* create a module Gameboard */
 
 const GameBoard = (() => {
-
     // create an array with 9 elements that will be the gameborard sqauares
     let gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
@@ -13,20 +12,52 @@ const GameBoard = (() => {
 
         /*for each element of gameBoard array add a div with class board_sqare and the id of the specific element */
         for (let i = 1; i <= gameBoard.length; i++)  {
-            boardHTML+=`<div class="board_square" id=square-${i}></div>`
-            
-           
-        }
-
-          
+            boardHTML+=`<div class="board_square" id=square-${i}></div>`         
+        }     
         document.querySelector("#gameboard").innerHTML = boardHTML;
     }
-
-   
-
     return {
         createGameBoard,
     }
+})();
+
+
+// use a factory to create players because we have multiple players
+
+const createPlayers = (name, symbol) => {
+    return { name, symbol };
+
+}
+
+
+
+/* create the logic to start the game  with the module StartGame*/
+
+const StartGame = (() => {
+
+    let Players = [];  /*array that holds the 2 players */
+    let currentPlayerIndex;
+
+    //function to start the game
+    const start = () => {
+        //the create of Player Objects
+        Players = [
+            createPlayers(document.querySelector("#player1").value, "X"),
+            createPlayers(document.querySelector("#player2").value, "O"),
+        ]
+        //set the currentPlayerIndex
+        currentPlayerIndex = 0;
+        // call the crateGameBoard here
+
+        console.log(Players, currentPlayerIndex);
+        GameBoard.createGameBoard();
+    }
+
+    //export start to be used outside the module
+   return {
+       start,
+    }
+
 
 
 })();
@@ -36,8 +67,8 @@ const GameBoard = (() => {
 
 let startGameButton = document.querySelector("#start_game");
 startGameButton.addEventListener("click", () => {
-
-    GameBoard.createGameBoard();
+    StartGame.start();
+   
 
 }
 
