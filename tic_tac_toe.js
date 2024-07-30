9a59d89704e9a0a349bef2feb6c7a8cef20e3b42
Alketa Memaj <memajalketa92@gmail.comgit config --global user.email memajalketa92@gmail.comgit config --global user.name Alketa>
2024-07-30 10:31:41 +0200

    Create a Module GameBoard with an array of 9 sqare elements and a function to create the game dashboard
     The module GameBoard contains an array with 9 sqares; another private module gameBoard with the function to create the 9 square divs; return the private function createGameBoard; call it when the user click the button startGame;

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,10 +1,43 @@
 // JavaScript source code
 
-let startGameButton = document.getElementById("start_game");
+/* create a module Gameboard */
 
+const GameBoard = (() => {
+
+    // create an array with 9 elements that will be the gameborard sqauares
+    let gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+
+    // create a function to create a 9 square grid when the user click start
+    const createGameBoard = () => {
+        let boardHTML = "";
+
+        /*for each element of gameBoard array add a div with class board_sqare and the id of the specific element */
+        for (let i = 1; i <= gameBoard.length; i++)  {
+            boardHTML+=`<div class="board_square" id=square-${i}></div>`
+            
+           
+        }
+
+          
+        document.querySelector("#gameboard").innerHTML = boardHTML;
+    }
+
+   
+
+    return {
+        createGameBoard,
+    }
+
+
+})();
+
+
+
+
+let startGameButton = document.querySelector("#start_game");
 startGameButton.addEventListener("click", () => {
 
-   console.log("hello")
+    GameBoard.createGameBoard();
 
 }
 
