367053ef0e1925a1e25bf863de5b75f3fe5ff5e3
Alketa Memaj <memajalketa92@gmail.comgit config --global user.email memajalketa92@gmail.comgit config --global user.name Alketa>
2024-07-30 23:10:41 +0200

    At the start function add an event listener to all the sqares that the user click
    console.log for every square, x for index player 0 and O for index player 1

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -12,7 +12,7 @@ const GameBoard = (() => {
 
         /*for each element of gameBoard array add a div with class board_sqare and the id of the specific element */
         for (let i = 1; i <= gameBoard.length; i++)  {
-            boardHTML+=`<div class="board_square" id=square-${i}></div>`         
+            boardHTML+=`<div class="board_square" id=${i}></div>`         
         }     
         document.querySelector("#gameboard").innerHTML = boardHTML;
     }
@@ -36,7 +36,8 @@ const createPlayers = (name, symbol) => {
 const StartGame = (() => {
 
     let Players = [];  /*array that holds the 2 players */
-    let currentPlayerIndex;
+    let currentPlayerIndex = 1;
+    let currentPlayerSymbol;
 
     //function to start the game
     const start = () => {
@@ -45,12 +46,33 @@ const StartGame = (() => {
             createPlayers(document.querySelector("#player1").value, "X"),
             createPlayers(document.querySelector("#player2").value, "O"),
         ]
-        //set the currentPlayerIndex
-        currentPlayerIndex = 0;
+     
+    
         // call the crateGameBoard here
 
         console.log(Players, currentPlayerIndex);
         GameBoard.createGameBoard();
+
+        // create an event listener click every time the user click in one of the gameboard squares and print the sqare id
+        const squares = document.querySelectorAll(".board_square");
+        squares.forEach((square) => {
+            square.addEventListener("click", () => {
+               
+                if (currentPlayerIndex == 0) {
+                    currentPlayerIndex = 1;
+                    currentPlayerSymbol = Players[currentPlayerIndex].symbol;
+
+                }
+
+                else if (currentPlayerIndex == 1) {
+                    currentPlayerIndex = 0;
+                    currentPlayerSymbol= Players[currentPlayerIndex].symbol;
+                }
+
+                console.log(square.id, currentPlayerIndex, currentPlayerSymbol);
+            }
+            );
+        });
     }
 
     //export start to be used outside the module
